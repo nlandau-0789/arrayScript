dee7c52b46e088bda0ebbcf881c0224ac7373ece
Nathan Landau <nathan.landau1234@gmail.com>
2023-03-24 22:29:12 +0100

    switched to new architecture

--- main_compiler.py
+++ main_compiler.py
@@ -1,6 +1,6 @@
 from lexer_compilator import get_compilator
 
-with open("ctest/cexemple.arrs", "r", encoding="utf-8") as f:
+with open("exemple.arrs", "r", encoding="utf-8") as f:
     code = f.read()
 
 with open("lexer.py", "w", encoding="utf-8") as f:
