2c37226793df051164a01b1ca52c01d9cb3001d8
Nathan Landau <nathan.landau1234@gmail.com>
2023-04-07 23:21:49 +0200

    added base arrs.h maker

--- make_arrs_h.py
+++ make_arrs_h.py
@@ -1,12 +1,13 @@
 from base_compiler import types, operators, funcs
 
-arrs_h = open("arrs.h", "w", encoding="utf-8")
+arrs_h = open("arrs.h", "x", encoding="utf-8")
 
 
 arrs_h.write("""
 namespace arrs {
 """)
 
+# begin operators namespace
 arrs_h.write("""
 namespace operators {
 """)
@@ -14,16 +15,56 @@ namespace operators {
 for i in operators:
     arrs_h.write(\
 f"""
-template<{", ".join("typename "+chr(65+i) for i in range(i.nb_args))}>
-auto {i.__name__}({", ".join(chr(65+i)+" "+chr(97+i) for i in range(i.nb_args))}){{
+template<{", ".join("typename "+chr(65+i) for i in range(i.nb_args-1))}>
+auto {i.__name__}({", ".join(chr(65+i)+" "+chr(97+i) for i in range(i.nb_args-1))}){{
     
 }}
 """
 )
 
+arrs_h.write("""
+}""")
+# end operators namespace
+
+# begin types namespace
+arrs_h.write("""
+namespace types {
+""")
+
+for i in types:
+    arrs_h.write(\
+f"""
+class {i.__name__}{{
+public :
+    {i.__name__}(){{}}
+    ~{i.__name__}(){{}}
+}};
+"""
+)
 
 arrs_h.write("""
 }""")
+# end types namespace
+
+# begin funcs namespace
+arrs_h.write("""
+namespace funcs {
+""")
+
+for i in funcs:
+    arrs_h.write(\
+f"""
+class {i}{{
+public :
+    {i}(){{}}
+    ~{i}(){{}}
+}};
+"""
+)
+
+arrs_h.write("""
+}""")
+# end funcs namespace
 
 arrs_h.write("""
 }""")
